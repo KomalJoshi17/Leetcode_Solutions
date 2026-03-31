2039abff683974d0d53f4f6a9649c8bf283f9963
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-03-31 21:51:23 +0530

    Time: 0 ms (100%), Space: 21.9 MB (74.2%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // vector<int>array;
+    // vector<int> sorted(TreeNode* root){
+    //     if(root==nullptr) return {};
+    //     sorted(root->left);
+    //     array.push_back(root->val);
+    //     sorted(root->right);
+    //     return array;
+    // }
+
+    // bool isSorted(vector<int> &arr){
+    //     for(int i=0;i<arr.size()-1;i++){
+    //         if(arr[i]>=arr[i+1]) return false;
+    //     }
+    //     return true;
+    // }
+
+    // bool isValidBST(TreeNode* root) {
+    //     vector<int>arr=sorted(root);
+    //     return isSorted(arr);
+    // }
+
+    bool validate(TreeNode* root,long long min, long long max){
+        if(root==nullptr) return true;
+
+        if(root->val <= min || root->val >= max) return false;
+
+        return validate(root->left,min,root->val) && validate(root->right,root->val,max);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return validate(root, LONG_MIN, LONG_MAX);
+    }
+};
