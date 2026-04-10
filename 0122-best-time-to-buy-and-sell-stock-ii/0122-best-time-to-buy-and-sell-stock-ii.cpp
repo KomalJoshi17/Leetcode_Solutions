689eca7bac244b08dfac7e75d97e2f36fb22f2ee
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-04-10 12:39:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,34 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         if (prices.empty()) return 0;
+        
+//         int minPrice = prices[0];
+//         int maxProfit = 0;
+
+//         for (int i = 1; i < prices.size(); ++i) {
+//             if (prices[i] < minPrice) {
+//                 minPrice = prices[i];
+//             } else {
+//                 maxProfit = max(maxProfit, prices[i] - minPrice);
+//             }
+//         }
+
+//         return maxProfit;
+//     }
+// };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] > prices[i-1]) {
+                maxProfit += prices[i] - prices[i-1];
+            }
+        }
+
+        return maxProfit;
+    }
+};
