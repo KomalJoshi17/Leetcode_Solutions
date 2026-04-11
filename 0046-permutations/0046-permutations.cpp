d6ef951b62fee8ac8400a422e7674927a082980a
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-04-11 22:37:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        
-    }
-};class Solution {
-public:
     void findPermutation(vector<int>& nums, set<vector<int>>&ans, int i){
-        
+    
         if(i>=nums.size()){
             ans.insert(nums);
             return;
         }    
-       
+     
         for(int j=i; j<nums.size(); j++){
-          
+            // Swap
             swap(nums[i], nums[j]);
-       
+            // Recusrive call
             findPermutation(nums, ans, i+1);
             // Backtracking - to recreate the original input string
             swap(nums[i], nums[j]);
