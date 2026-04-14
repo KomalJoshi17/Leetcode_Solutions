721fe4b9207aa65498f1ab46cbfce1ff9937533e
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-04-14 22:31:47 +0530

    Time: 0 ms (100%), Space: 8.9 MB (61.57%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -8,44 +8,44 @@ public:
         char carry='0';
         while(s1 >= 0 || s2 >= 0){
 
-        char bit1 = (s1 >= 0) ? a[s1] : '0';
-        char bit2 = (s2 >= 0) ? b[s2] : '0';
+            char bit1 = (s1 >= 0) ? a[s1] : '0';
+            char bit2 = (s2 >= 0) ? b[s2] : '0';
 
-        if(carry=='0' && bit1=='1' && bit2=='1'){
-            ans+='0';
-            carry='1';
+            if(carry=='0' && bit1=='1' && bit2=='1'){
+                ans+='0';
+                carry='1';
+            }
+            else if(carry=='1' && bit1=='1' && bit2=='0'){
+                ans+='0';
+                carry='1';
+            }
+            else if(carry=='1' && bit1=='0' && bit2=='1'){
+                ans+='0';
+                carry='1';
+            }
+            else if(carry=='1' && bit1=='1' && bit2=='1'){
+                ans+='1';
+                carry='1';
+            }
+            else if(carry=='0' && bit1=='0' && bit2=='1'){
+                ans+='1';
+                carry='0';
+            }
+            else if(carry=='0' && bit1=='1' && bit2=='0'){
+                ans+='1';
+                carry='0';
+            }
+            else if(carry=='0' && bit1=='0' && bit2=='0'){
+                ans+='0';
+                carry='0';
+            }
+            else if(carry=='1' && bit1=='0' && bit2=='0'){
+                ans+='1';
+                carry='0';
+            }
+            s1--;
+            s2--;
         }
-        else if(carry=='1' && bit1=='1' && bit2=='0'){
-            ans+='0';
-            carry='1';
-        }
-        else if(carry=='1' && bit1=='0' && bit2=='1'){
-            ans+='0';
-            carry='1';
-        }
-        else if(carry=='1' && bit1=='1' && bit2=='1'){
-            ans+='1';
-            carry='1';
-        }
-        else if(carry=='0' && bit1=='0' && bit2=='1'){
-            ans+='1';
-            carry='0';
-        }
-        else if(carry=='0' && bit1=='1' && bit2=='0'){
-            ans+='1';
-            carry='0';
-        }
-        else if(carry=='0' && bit1=='0' && bit2=='0'){
-            ans+='0';
-            carry='0';
-        }
-        else if(carry=='1' && bit1=='0' && bit2=='0'){
-            ans+='1';
-            carry='0';
-        }
-        s1--;
-        s2--;
-    }
 
         if(carry=='1') ans+='1';
         reverse(ans.begin(),ans.end());
