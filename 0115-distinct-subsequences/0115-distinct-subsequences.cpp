4bf46c56ae85ccc15160979a8339e1e0414b0777
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-05-05 21:08:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -29,32 +29,3 @@ public:
         return helper(s, t, 0, 0);
     }
 };
-
-
-
-/*
-TC: O(S + 2^T)
-
-int helper(string& s, string& t, int i, int j){
-    if(i == s.size() and j == t.size()){
-        return 1;
-    }
-    if(i >= s.size()){
-        return 0;
-    }
-
-
-    int not_take = helper(s, t, i+1, j);
-    int take = 0;
-    if(s[i] == t[j]){
-        take = helper(s, t, i+1, j+1);
-    }
-
-    return take + not_take;
-}
-
-int numDistinct(string s, string t) {
-    return helper(s, t, 0, 0);
-}
-
-*/
