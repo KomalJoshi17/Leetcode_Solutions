c26d57aa01597e6f4882b3af1ad4201dfbfb5b05
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-07-08 15:21:30 +0530

    Time: 0 ms (100%), Space: 13.1 MB (86.26%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 0)
-            return;
-
-        grid[i][j] = 1;
-        dfs(i+1, j, grid);
-        dfs(i-1, j, grid);
-        dfs(i, j+1, grid);
-        dfs(i, j-1, grid);
-    }
-    
-    int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        for (int i = 0; i < m; i++) {
-            dfs(i, 0, grid);
-            dfs(i, n-1, grid);
-        }
-        for (int j = 0; j < n; j++) {
-            dfs(0, j, grid);
-            dfs(m-1, j, grid);
-        }
+    void dfs(int i, int j, vector<vector<int>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
         
-        int count = 0;
-        for (int i = 1; i < m-1; i++) {
-            for (int j = 1; j < n-1; j++) {
-                if (grid[i][j] == 0) {
-                    dfs(i, j, grid);
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]!=0) return;
+        grid[i][j]=1;
+
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i,j-1,grid);
+    }
+
+    int closedIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+
+        for(int i=0;i<n;i++){
+            dfs(i,0,grid);
+            dfs(i,m-1,grid);
+        }
+
+        for(int j=0;j<m;j++){
+            dfs(0,j,grid);
+            dfs(n-1,j,grid);
+        }
+
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
+                    dfs(i,j,grid);
                     count++;
                 }
             }
         }
+
         return count;
     }
 };
