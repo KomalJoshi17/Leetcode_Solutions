5573e86f235ee118dc947e0259c6ced2d1394036
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-07-15 13:47:46 +0530

    Time: 0 ms (100%), Space: 19.5 MB (61.38%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int>p, r;
+    int find(int i){
+        if(p[i]==i) return i;
+        return p[i]=find(p[i]); 
+    }
+
+    bool Union(int x, int y){
+        int px=find(x);
+        int py=find(y);
+        
+        if(px==py) return false;
+
+        if(r[px]>r[py]){
+            p[py]=px;
+        }else if(r[px]<r[py]){
+            p[px]=py;
+        }else{
+            p[py]=px;
+            r[px]++;
+        }
+        return true;
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        p.resize(n);
+        r.resize(n,1);
+        for(int i=0;i<n;i++){
+            p[i]=i;
+        }
+
+        int ans=n;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+                    if(Union(i,j)){
+                        ans--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
