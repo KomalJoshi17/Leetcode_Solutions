df1659c3bedf3edcb5d315947f9dcf6cb636a820
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-08-06 14:42:47 +0530

    Time: 175 ms (49.36%), Space: 157.3 MB (33.4%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -5,12 +5,9 @@ public:
 
         for (int i = 0; i < nums.size(); i++) {
             auto it = st.lower_bound((long long)nums[i] - valueDiff);
-
             if (it != st.end() && *it <= (long long)nums[i] + valueDiff)
                 return true;
-
             st.insert(nums[i]);
-
             if (st.size() > indexDiff)
                 st.erase(st.find(nums[i - indexDiff]));
         }
