30beca9b87875f4b94bcf82e5b7fc6e8153e8d5c
KomalJoshi17 <145208745+KomalJoshi17@users.noreply.github.com>
2026-09-20 20:17:12 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.88%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int size=s.size()-1;
+
+        while(size>=0 && s[size]==' '){
+            size--;
+        }
+        int count=0;
+        while(size>=0 && s[size]!=' '){
+            count++;
+            size--;
+        }
+        return count;
+    }
+};
